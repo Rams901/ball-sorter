b4af0bc2b523230f95e49b9e244371b5ec1f1d33
Ramsis Hammadi <ramsishammadi@Ramsiss-MacBook-Air.local>
2024-01-10 20:41:12 +0100

    Model Learning

--- BallSorter_class.py
+++ BallSorter_class.py
@@ -1,78 +1,14 @@
 import random
 import gymnasium as gym
+
 from gymnasium import spaces
 import numpy as np
-from collections import deque
-import cv2
+
 from ball_sorter_op import optimal_n_moves
 
-N_BALLS = 5
+N_BALLS = 4
 
-def no_balls_foul(observation, curr_position):
 
-    if observation[curr_position] == -1:
-        print("No Balls in current position")
-        return 1
-    return 0
-
-def out_of_bound(observation, curr_position, move):
-   
-    if ((curr_position+move) >= len(observation) or (curr_position+move) < 0):
-            print("Out Of Bound Error")
-            return 1
-
-    return 0
-
-# Penalize if a move is giving a ball to one that has more balls than it does
-def ball_to_greater_bucket(observation, curr_position, move):
-    
-    if observation[curr_position] < observation[curr_position+move]:
-        return 1
-    
-    return 0
-
-def filled_balls(observation, curr_position):
-        
-        int("The balls are filled equally")
-        observation = [0] * (len(observation))
-        observation.insert(curr_position, len(observation))
-        observation = np.array([observation])
-        
-        return observation
-
-def compare_to_optimal_n_moves(moves, optimal_moves):
-    return optimal_moves - moves
-
-# Rewards
-def reward_system(observation, curr_position, move):
-    
-    reward = 0
-    valid = 1
-    if move:
-        
-        if out_of_bound(observation, curr_position, move):
-            reward -= 10
-            valid = 0
-        
-        elif no_balls_foul(observation, curr_position):
-            reward -= 10
-            valid = 0
-        
-        elif ball_to_greater_bucket(observation, curr_position, move):
-            reward -= 5
-
-        else:
-            reward -= 1
-    
-    else:
-
-        if observation[curr_position] > 1:
-            reward -= 3
-
-    # Correlating to the variance to number of optimal moves (might need to adjust after tracking results)
-    # reward += compare_to_optimal_n_moves(observation[0], optimal_moves)
-
-    return reward, valid
 
 # Switch + Check if it's Reaching Bounding Box.
 # What if it only chooses a ball position
@@ -97,6 +33,7 @@ def reward_system(observation, curr_position, move):
 # No move, No move, Right, No move -> No Move, No move, Left, No Move -> No move, left, left, No move -> 1, 1, 1, 1.
 # on each round, it will iterate through the different positions! Awesome.
 
+# Sealing
 def swap_ball(observation, curr_position, move):
     
 
@@ -106,42 +43,97 @@ def swap_ball(observation, curr_position, move):
     
     return observation
 
-def switch_ball(observation, curr_position, left):
-    
-    score = 0
-
-    if left:
-        try:
-            switch_ind = curr_position - 1
-            observation[switch_ind] += 1
-        except Exception as e:
-            score -= 10
-    
-    else:
-        try:
-            switch_ind = curr_position + 1
-            observation[switch_ind] += 1
-        except Exception as e:
-            score -= 10
-    
-    observation[curr_position] -= 1
-
-    return observation, score
-
 class BallSorter(gym.Env):
 
     def __init__(self):
-
         super(BallSorter, self).__init__()
         self.seed = 0
+        self.debug_state = False
+
         # It can either add a ball to the right, left
         # or switch position to another one
         self.n = N_BALLS
         self.moves = 0
         self.action_space = spaces.Discrete(3)
 
-        self.observation_space = spaces.Box(low = 0, high = self.n, shape = (self.n, ), dtype = np.int8)
+        self.observation_space = spaces.Box(low = 0, high = self.n, shape = (self.n, ), dtype = np.int32)
+    def print_debug_info(self, *x):
+        
+        if self.debug_state:
+            print(", ".join(x))
 
+    def no_balls_foul(self, observation, curr_position):
+    
+        if observation[curr_position] == 0:
+    
+            self.print_debug_info("No Balls in current position")
+    
+            return 1
+    
+        return 0
+
+    def out_of_bound(self, observation, curr_position, move):
+    
+        if ((curr_position+move) >= len(observation) or (curr_position+move) < 0):
+        
+                self.print_debug_info("Out Of Bound Error")
+        
+                return 1
+        
+        return 0
+
+# Penalize if a move is giving a ball to one that has more balls than it does
+    def ball_to_greater_bucket(self, observation, curr_position, move):
+        
+        if observation[curr_position] < observation[curr_position+move]:
+            return 1
+        
+        return 0
+
+    def filled_balls(self, observation, curr_position):
+            
+            int("The balls are filled equally")
+            observation = [0] * (len(observation))
+            observation.insert(curr_position, len(observation))
+            observation = np.array([observation])
+            
+            return observation
+
+    def compare_to_optimal_n_moves(self, moves, optimal_moves):
+        return optimal_moves - moves
+
+    # Rewards
+    def reward_system(self, observation, curr_position, move):
+        
+        reward = 0
+        valid = 1
+
+        if move:
+            
+            if self.out_of_bound(observation, curr_position, move):
+                reward -= 10
+                valid = 0
+            
+            elif self.no_balls_foul(observation, curr_position):
+                reward -= 10
+                valid = 0
+            
+            elif self.ball_to_greater_bucket(observation, curr_position, move):
+                reward -= 2
+    # 2, 3, 0, 0, 0 
+            else:
+                reward -= 1
+        
+        else:
+
+            if observation[curr_position] > 1:
+                reward -= 10
+        # it seems to converge to not choosing any move for more rewards
+        # Correlating to the variance to number of optimal moves (might need to adjust after tracking results)
+    # reward += compare_to_optimal_n_moves(observation[0], optimal_moves)
+
+        return reward, valid
+    
     def step(self, action):
         
         # Execute actions:
@@ -171,42 +163,42 @@ class BallSorter(gym.Env):
             case 2:
                 move = -1
 
-        loc_reward, valid = reward_system(self.obs, self.curr_position, move)
-        print(f"action: {action}, reward: {loc_reward}, valid: {valid}, current position: {self.curr_position}")
+        loc_reward, valid = self.reward_system(self.obs, self.curr_position, move)
+        
+        self.print_debug_info(f"action: {action}, local reward: {loc_reward}, valid: {valid}, current position: {self.curr_position}")
+
         if valid:
             self.obs = swap_ball(self.obs, self.curr_position, move)
-        print(self.obs, np.ones(shape = (len(self.obs,),), dtype = np.int8))
+        
+        self.print_debug_info(self.obs, np.ones(shape = (len(self.obs,),), dtype = np.int8))
+        
         if  (self.obs == 1).all():
             loc_reward += 100
             self.done = True
-            print("sorting is done")
+            self.print_debug_info("sorting is done")
 
-        self.total_reward = loc_reward
-        self.reward = self.total_reward - self.prev_reward
-        self.prev_reward = loc_reward
-        
 
-        if self.curr_position == (self.n - 2):
+    
+        if self.curr_position == (self.n - 1):
             self.curr_position = 0
         else:
             self.curr_position += 1
         
 
-        return self.obs, self.reward, self.done, False, {}
+        return self.obs, loc_reward, self.done, False, {}
     
     def reset(self, seed = 0):
 
         self.curr_position = random.randint(0, self.n-1)
         self.optimal_moves = optimal_n_moves(self.n, self.curr_position)
         self.done = False
-        self.prev_reward = 0
 
         observation  = [0] * (self.n)
         observation[self.curr_position] = self.n
         observation = np.array(observation, dtype = np.int8)
         
         self.obs = observation
-        print(f"OBSERVATION AT RESET: {observation}\nCurr Position: {self.curr_position}\nOptimal Moves: {self.optimal_moves}")
+        self.print_debug_info(f"OBSERVATION AT RESET: {observation}\nCurr Position: {self.curr_position}\nOptimal Moves: {self.optimal_moves}")
         return observation, {"info": ""}
 
         # We either switch or take a ball from the curr_position to the next
--- space_experiment.py
+++ space_experiment.py
@@ -1,12 +1,13 @@
 from BallSorter_class import BallSorter
 env = BallSorter()
-episodes = 50
+episodes = 3
 
 for episode in range(episodes):
 	done = False
 	obs = env.reset()
-	while True:#not done:
+	for i in range(10):#not done:
 		random_action = env.action_space.sample()
 		print("action",random_action)
 		obs, reward, done, _, info = env.step(random_action)
 		print('reward',reward)
+	
